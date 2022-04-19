bc92494fc7ef173599194670243872aa63582f28
Valeriia Neskoromnaya <81985957+VNeskoromnaya@users.noreply.github.com>
2022-04-19 20:41:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+/*  Логика: 
+задаем диапазон чисел, выставляем количество чисел в поле (которые должны отражаться), 
+нажимаем на кнопку "Сгенерировать " и в div "result" выводятся цифры, которые также попадают в массив 
+для дальнейшей работы с ними
+
+очистить поле div "result" после обновления страницы
+*/
+
+const from = document.getElementById('from'),
+    to = document.getElementById('to'),
+    numOfGenerated = document.getElementById('numOfGenerated'),
+    resultRandom = document.getElementById('resultRandom'),
+    btn = document.getElementById('btn'),
+    addData = document.getElementById('addData');
+    
+let arr = [];
+
+function getRandom() {
+
+    // выведение     
+    let min = parseInt(from.value);
+    let max = parseInt(to.value);
+    let num = min + Math.floor(Math.random() * (max-min)); // такая формула позволяет включать в массив и отр числа
+    resultRandom.innerHTML += (num + '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+
+    arr.push(num);
+}
+
+function mathOperations() {
+    // выводим минимальное число из полученного массива
+    let minNum = Math.min.apply(null, arr);
+    
+    // выводим максимальное число из полученного массива
+    let maxNum = Math.max.apply(null, arr);
+    
+    // выводим сумму чисел массива среднее арифметическое чисел из массива
+    let sum = arr.reduce((a, b) => a + b, 0); 
+
+    // выводим среднее арифметическое чисел из массива
+    let avarageSum = sum / arr.length; // Делим её на длину массива
+
+    // выводим произведение чисел массива
+    let product = arr.reduce((a, b) => a * b);
+
+    addData.innerHTML = `Для информации из приведенных выше чисел: <br/>
+                        Минимальное число: ${minNum} <br />
+                        Максимальное число: ${maxNum} <br />
+                        Среднее арифметическое: ${avarageSum} <br />
+                        Сумма чисел: ${sum} <br />
+                        Произведение чисел: ${product} <br />`;
+}
+
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    resultRandom.innerHTML = '';    
+    arr = [];
+    // сделать проверку numOfGenerated.value что это число
+    for(let i=0; i<Number(numOfGenerated.value); i++) {
+        getRandom();      
+    }
+    mathOperations();
+});
+
